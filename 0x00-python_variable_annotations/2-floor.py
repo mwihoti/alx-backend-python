4ded5a6cd80d2b110a1bdaa73ca8bf15f51855d1
Danmwihoti <danmwihoti@gmail.com>
2024-08-01 14:50:06 +0300

    basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function floor that takes a float n as arguement and returns floor as float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float number.
+    """
+    return int(n)
