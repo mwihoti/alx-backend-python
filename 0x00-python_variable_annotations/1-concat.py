ec4055fe2b6568ac81d7536bed6bc860cf841b6c
Danmwihoti <danmwihoti@gmail.com>
2024-08-01 14:38:56 +0300

    Basic annotations -concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+Function that concatenates str1 and str2 as string and returns
+a concated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatenation of two strings.
+    """
+    return str1 + str2
