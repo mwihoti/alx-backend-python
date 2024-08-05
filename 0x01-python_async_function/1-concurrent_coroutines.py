e18ba466a35779c9cb4ab53f939948a9d7610592
Danmwihoti <danmwihoti@gmail.com>
2024-08-05 12:43:14 +0300

    correct pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,11 +8,11 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n( n : int, max_delay: int)-> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Executes multiple coroutines at same time
     """
     times = await asyncio.gather(
-        *(wait_random(max_delay) for _ in  range(n))
+        *(wait_random(max_delay) for _ in range(n))
     )
     return sorted(times)
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Function task_wait_random that takes an integer max_delay and returns
+a asyncio.Task
+"""
+import time
+import asyncio
