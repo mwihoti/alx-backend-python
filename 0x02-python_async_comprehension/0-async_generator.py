e093ef8db94701551ea6cd216e037b524a3801ec
Danmwihoti <danmwihoti@gmail.com>
2024-08-06 19:04:42 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Coroutine function that takes no arguement
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Coroutine that generates random numbers between 0 and 10, one number
+    at a time.
+    Yields:
+        Generator[float, None, None]:  A random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,8 @@
+Project name:
+0x02. Python - Async Comprehension
+
+Author:
+Mwihoti
+
+Project description:    
+Asynchronous Comprehensions and Asynchronous Generators
