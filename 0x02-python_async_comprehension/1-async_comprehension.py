13d51c132328112f1b5974000ffcf716388b3571
Danmwihoti <danmwihoti@gmail.com>
2024-08-06 19:26:05 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+coroutine async_comprehension
+"""
+import asyncio
+async_generator = __import__('1-async_generator').async_generator
+
+
+async def async_comprehension():
+    
