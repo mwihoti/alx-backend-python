14eeabf8585a3344f8ef3a0781766150ed8a102f
Danmwihoti <danmwihoti@gmail.com>
2024-08-06 19:48:36 +0300

    coroutine async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Coroutine function that takes no arguement
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that generates random numbers between 0 and 10, one number
     at a time.
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,16 +3,15 @@
 coroutine async_comprehension
 """
 import asyncio
-from importlib import import_module 
+from typing import List
+from importlib import import_module as use
 
-async_generator = import_module('0-async_generator').async_generator
+async_generator = use('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     coroutine will collect 10 random numbers using an async comprehensing
     over async_generator, then return the 10 random numbers
     """
-    return [rad async for rad in async_generator()]
-    
-    
+    return [rad async for rad in async_generator()]   
