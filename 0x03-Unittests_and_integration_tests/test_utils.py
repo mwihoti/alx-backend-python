b4e087bff686509d08e72dcb43c53ad72115cbfb
Danmwihoti <danielmwihoti@gmail.com>
2024-08-26 21:16:12 +0300

    unittest pycode

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,11 +5,11 @@ UnitTest utils.access_nested_map
 """
 import unittest
 from parameterized import parameterized
-from utils import (access_nested_map)
+from utils import (access_nested_map, get_json)
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    
+
     """
     Class TestAccessNestedMap
     """
@@ -22,5 +22,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         Test that the method that returns what is supposed to
         """
-        
-        
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """
+        Test that that method raises expected exception
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    def class TestGetJson(unittest.TestCase)
+    """
+    def test_get_json(self, test_url, test_payload):
+        """
+        Checks  get_json returns expected results
+        """
