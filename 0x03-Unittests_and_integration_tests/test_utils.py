af106e0589e477922e2aa3d4c01557672da52bc6
Danmwihoti <danielmwihoti@gmail.com>
2024-08-26 21:41:16 +0300

    class TestMemoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ UnitTest utils.access_nested_map
 """
 import unittest
 from parameterized import parameterized
-from utils import (access_nested_map, get_json)
+from utils import (access_nested_map, get_json, memoize)
 from unittest.mock import patch
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -55,3 +55,29 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(test_url), test_payload)
         mock.assert_called_once()
         patcher.stop()
+
+class TestMemoize(unittest.TestCase):
+    """
+    Defines class TestMoize
+    """
+    
+    def test_memoize():
+        """
+         Tests the function when calling a_property twice,
+         the correct result is returned but a_method is only
+         called once using assert_called_once
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()           
